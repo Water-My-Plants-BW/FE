6e554c4445d3f65537ce71dd4488df3d70877841
bhawnishk1 <mrbhawnish@gmail.com>
2020-07-30 15:39:45 -0700

    changes for netlify

---
+++ build/precache-manifest.bf3d64a37a1184429db4bd372fe7dbe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ffa979596eb08cd207806ed00d8fd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9652ce426d856dcd8497",
+    "url": "/static/js/2.b457bd4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b457bd4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8a1c73cc1260db03a6",
+    "url": "/static/js/main.4d4cad41.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);

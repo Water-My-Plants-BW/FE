2794392b22e369cee6ec09ca2f234f281f2303b5
unknown <cromer.dalton@gmail.com>
2020-07-29 21:49:54 -0400

    added siyuan's changes

--- sprout/src/components/EditProfile.js
+++ sprout/src/components/EditProfile.js
@@ -2,19 +2,21 @@ import React from "react";
 import axios from "axios";
 import styled from "styled-components";
 import { withRouter } from "react-router-dom";
+
 class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
       id: "",
-      phone: "",
+      phoneNumber: "",
       password: "",
     };
   }
   componentDidMount() {
-    let id = localStorage.getItem(`id`);
-    const url = `https://lambda-sprout.herokuapp.com/api/user/${id}`
+    let id = localStorage.getItem(`userId`);
+    const url = `https://lambda-sprout.herokuapp.com/api/users/${id}`
+
     this.setState({ id: id });
     try {
       axios
@@ -22,21 +24,39 @@ class UserProfile extends React.Component {
         .then((res) => {
           this.setState({
             username: res.data.username,
-            phone: res.data.phone,
+            phoneNumber: res.data.phoneNumber,
           });
         });
     } catch (err) {
       console.log(err);
     }
   }
+
   render() {
     return (
-      <div>
-        <h3>Name : {this.state.username}</h3>
-        <h3>Phone : {this.state.phone}</h3>
-      </div>
+        <UserWrapper>
+        <UserBar>
+             <h3>Name : {this.state.username}</h3>
+             <h3>Phone# : {this.state.phoneNumber}</h3>
+        </UserBar>
+        </UserWrapper>
     );
-  }
 }
+}
+    
 export default withRouter(UserProfile);
+
+const UserWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`
+const UserBar = styled.div`
+box-shadow: 0px 2px 2px purple;
+text-align : center;
+width: 400px;
+border-radius: 5px;
+padding-top: 60px;
+padding-bottom: 60px;
+margin: 50px auto;
+`
 //withRouter gives component access to this.props.history, which means the component redirect the user.
--- sprout/src/components/User.js
+++ sprout/src/components/User.js
@@ -1,46 +1,53 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
 import { withRouter } from "react-router-dom";
+import img from "../img/user.jpg"
+
 class User extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
-      id: "",
-      phone: "",
+      phoneNumber: "",
       password: "",
     };
   }
   componentDidMount() {
-    let id = localStorage.getItem(`id`);
-    const url = `https://lambda-sprout.herokuapp.com/api/user/${id}`;
+    let id = localStorage.getItem(`userId`);
+    const url = `https://lambda-sprout.herokuapp.com/api/users/${id}`;
     this.setState({ id: id });
+    
       // The try statement lets you test a block of code for errors. The catch statement lets you handle the error. 
       try {
         axios
             .get(url, { headers: { Authorization: localStorage.getItem("token") } })
             .then(res => {
-                this.setState({ username: res.data.username,  phone: res.data.phone})
+                this.setState({ username: res.data.username,  phoneNumber: res.data.phoneNumber})
             })
     } catch (err) {
         console.log(err);
     }
 }
-  handleInput = (event) => {
+
+  handleInput = event => {
     event.preventDefault();
     this.setState({ [event.target.name]: event.target.value });
   };
+
   updateInfo = (data, id) => {
     data = {
       username: this.state.username,
       password: this.state.password,
-      phone: this.state.phone,
+      phoneNumber: this.state.phoneNumber,
     };
-    id = localStorage.getItem(`id`);
+    id = localStorage.getItem(`userId`);
     console.log(id);
     const url =
-      `https://lambda-sprout.herokuapp.com//api/user/${id}` ||
-              `http://localhost:3000/api/user/${id}`;
+      `https://lambda-sprout.herokuapp.com//api/users/${id}` ||
+
+              `http://localhost:3000/api/users/${id}`;
+
       // The try statement lets you test a block of code for errors. The catch statement lets you handle the error. The throw statement lets you create custom errors. 
       try{
       axios
@@ -51,22 +58,24 @@ class User extends React.Component {
           console.log(res);
           this.setState({
             username: res.data.username,
-            phone: res.data.phone,
+            phoneNumber: res.data.phoneNumber,
             password: res.data.password,
           });
           alert("Your Update Submitted Successfully");
-          console.log(res.data.phone);
-          this.props.history.push("/myplants");
+          console.log(res.data.phoneNumber);
+          this.props.history.push("/plants");
         });
     } catch (err) {
       console.log(err);
     }
   };
+
   deleteMyAccount = (id) => {
-    id = localStorage.getItem(`id`);
+    id = localStorage.getItem(`userId`);
     const url =
-    `https://lambda-sprout.herokuapp.com/api/user/${id}` ||
+    `https://lambda-sprout.herokuapp.com/api/users/${id}` ||
       `http://localhost:3000/api/users/${id}`;
+      
     alert("Your Account Will be deleted permanantly");
     try {
       axios.delete(url).then((res) => {
@@ -81,44 +90,94 @@ class User extends React.Component {
       console.log(err);
     }
   };
+
   render() {
     return (
-          <div className="loginform">
-            <form onSubmit={this.updateInfo}>
+      <Wrapper>
+      <UserBar>
+        <div className ='loginform'>
+          <form onSubmit= {this.updateInfo}>
               <input
-                className="input"
-                onChange={this.handleInput}
-                placeholder="name"
-                value={this.state.username}
-                name="username"
+                  className='input'
+                  onChange={this.handleInput}
+                  placeholder="name"
+                  value={this.state.username}
+                  name="username"
               />
               <input
-                className="input"
-                type="password"
-                onChange={this.handleInput}
-                placeholder=" new password"
-                value={this.state.password}
-                name="password"
+                  className='input'
+                  type= 'password'
+                  onChange={this.handleInput}
+                  placeholder=" new password"
+                  value={this.state.password}
+                  name="password"
               />
               <input
-                className="input"
-                onChange={this.handleInput}
-                placeholder="phone"
-                value={this.state.phone}
-                name="phone"
+                  className='input'
+                  onChange={this.handleInput}
+                  placeholder="phone#"
+                  value={this.state.phoneNumber}
+                  name="phone"
               />
-            </form>
-            <div className="btn">
-              <button onClick={this.deleteMyAccount}>
-                {" "}
-                Delete My Account permanantly
-              </button>
-              <button className="updateBtn" onClick={this.updateInfo}>
-                Update
-              </button>
-            </div>
+          </form>
+
+          <div className="btn">
+              <button onClick={this.deleteMyAccount}> Delete My Account permanantly</button>
+              <button className="updateBtn" onClick={this.updateInfo}>Update</button>
           </div>
-    );
-  }
+        
+          </div>
+      </UserBar>
+  </Wrapper>
+)
 }
+}
+
 export default withRouter(User);
+
+
+const Wrapper =styled.div`
+        background-image: url(${img});
+        background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+      width: 1500px;
+      height: 1000px;
+`
+const UserBar = styled.div`
+    box-shadow: 0px 2px 2px #9464FA;
+    text-align : center;
+    width: 400px;
+    border-radius: 5px;
+    padding-top: 60px;
+    padding-bottom: 60px;
+    margin: 50px auto;
+    .input{
+          margin: 5px;
+          height: 25px;
+          width : 300px;
+          border-radius: 5px;
+          border: none;
+          box-shadow: 0 2px 4px #272727;
+          text-align:center;
+          @media(max-width: 479px){
+              width: 250px;
+          }
+    }
+      button{
+        background-color: #009FB7;
+        border-radius: 5px;
+        color : white;
+        margin: 10px;
+        height: 30px;
+        border: none;       
+      }
+      button:hover{
+          box-shadow: 0 2px 4px #272727;
+          transform: scaleX(1.025) scaleY(1.025);
+          cursor : pointer;
+          transition: all 0.2s;
+      }
+      }
+    }
+  `
